ee6f60015636ee35e157e6c8eaa85acf745e1e63
Paul Dequidt <pdequidt@hotmail.com>
2023-09-23 13:34:42 +0200

    Datingapp23 Sectie 2

--- API/program.cs
+++ API/program.cs
@@ -10,12 +10,12 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
