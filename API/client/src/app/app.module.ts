5b48dfd6f13fadab1ed87a063c7809672db3712d
Paul Dequidt <pdequidt@hotmail.com>
2023-09-27 17:30:40 +0200

    Section 3 intermediate

--- API/client/src/app/app.module.ts
+++ API/client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http'; // Added for HttpClient
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MemberDetailComponent
   ],
   imports: [
     BrowserModule,
