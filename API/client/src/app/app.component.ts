bef386dafff2c04e0a6e4277bc1a13f6cc1b8c5c
Paul Dequidt <pdequidt@hotmail.com>
2023-10-02 16:58:13 +0200

    Section 3 -lesson 24

--- API/client/src/app/app.component.ts
+++ API/client/src/app/app.component.ts
@@ -12,15 +12,13 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-    this.getUsers();
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    })
-  }
-}
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe
+    (
+      {
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed'),
+      }
+    )
+}}
--- API/client/src/app/app.module.ts
+++ API/client/src/app/app.module.ts
@@ -2,18 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'; // Added for HttpClient
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MemberDetailComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule
   ],
   providers: [],
